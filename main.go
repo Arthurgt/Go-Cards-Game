1e8d23cb7cf68e9f8609572c1f949cb7be502aa1
Arthurgt <arturbabkiewicz@gmail.com>
2023-08-26 13:53:29 +0200

    Commit_2 - Playing with structures, created newCard() function

--- main.go
+++ main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello world")
+	cards := []string {"Six of diamonds", newCard()}
+	cards = append(cards, "Three of glances")
+	for index, card := range cards {
+		fmt.Println(index, card)
+	}
+}
+
+func newCard() string {
+	return "Five of diamonds"
 }
