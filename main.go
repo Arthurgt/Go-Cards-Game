c3e4c9595959b4db8c63173d94d923caca11619c
Arthurgt <arturbabkiewicz@gmail.com>
2023-08-27 15:54:45 +0200

    Commit_5 - Created to string function to deck type

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import "fmt"
 func main() {
 	cards := newDeck()
 	hand, remainingDeck := deal(cards, 5)
+	deckAsAstring := cards.toString()
+	fmt.Println("Deck as a single string")
+	fmt.Println(deckAsAstring)
 	fmt.Println("Your hand")
 	hand.print()
 	fmt.Println("Remainig deck")
